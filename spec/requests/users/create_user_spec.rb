9db36da1b5e7273d9dc0db992c426578cf73cfd6
stephen <malek.stephen44@gmail.com>
2018-12-08 13:35:52 -0700

    Write test to cover edge case if bad request

--- spec/requests/users/create_user_spec.rb
+++ spec/requests/users/create_user_spec.rb
@@ -21,4 +21,23 @@ describe 'POST /api/v1/users' do
     expect(user[:email]).to eq(body[:user][:email])
     expect(user[:token]).to be_a(String)
   end
+  it 'returns error if bad request' do
+    headers = {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+    }
+
+    body = {
+      user: {
+        email: "example@example.example"
+      }
+    }
+
+    post '/api/v1/users', params: body.to_json, headers: headers
+
+    result = JSON.parse(response.body, symbolize_names: true)
+
+    expect(result[:message]).to eq("Bad Request")
+    expect(response).to_not be_successful
+  end
 end
