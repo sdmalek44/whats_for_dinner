82a0b459540a6ec4195b7c2d507ebe30ec67056d
stephen <malek.stephen44@gmail.com>
2018-12-08 14:30:40 -0700

    Add request test for searches spec

---
+++ spec/requests/searches/user_searches_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'POST /users/:token/searches' do
+  it 'can create a new search and return search results' do
+    user_password = 'monkeys'
+    user = create(:user)
+
+    headers = {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+    }
+
+    body = {
+      key_word: "example@example.example",
+      allergies: ['peanut', 'dairy'],
+      max_cook_time: '15'
+    }
+
+    post "/api/v1/user/#{user.token}", params: body.to_json, headers: headers
+
+    recipes = JSON.parse(response.body, symbolize_names: true)
+    recipe = recipes.first
+
+    expect(recipes.length).to eq(10)
+    expect(recipe).to have_key(:name)
+    expect(recipe).to have_key(:image)
+    expect(recipe).to have_key(:ingredients)
+    expect(recipe).to have_key(:recipe_id)
+    expect(recipe).to have_key(:cook_time)
+  end
+end
