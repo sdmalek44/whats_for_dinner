d1bbf708639e8d47ae4e9ec0835a1b46015aa43c
stephen <malek.stephen44@gmail.com>
2018-12-08 13:55:57 -0700

    Add test for incorrect login

--- spec/requests/sessions/user_login_spec.rb
+++ spec/requests/sessions/user_login_spec.rb
@@ -24,4 +24,27 @@ describe 'POST /api/v1/login' do
     expect(user[:email]).to eq(body[:user][:email])
     expect(user[:token]).to be_a(String)
   end
+  it 'does not create user if password incorrect' do
+    user = create(:user)
+    user_token = user.token
+
+    headers = {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+    }
+
+    body = {
+      user: {
+        email: "example@example.example",
+        password: 'wrong'
+      }
+    }
+
+    post '/api/v1/login', params: body.to_json, headers: headers
+
+    user = JSON.parse(response.body, symbolize_names: true)
+
+    expect(user[:message]).to eq('Bad Request')
+    expect(response).to_not be_successful
+  end
 end
