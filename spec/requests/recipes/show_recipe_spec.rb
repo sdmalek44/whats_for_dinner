0e6e06e15dad61b67b2f82730e1b4267bf108887
stephen <malek.stephen44@gmail.com>
2018-12-09 08:17:07 -0700

    Add VCR to tests so it doesn't make actual api calls after the first time

--- spec/requests/recipes/show_recipe_spec.rb
+++ spec/requests/recipes/show_recipe_spec.rb
@@ -2,23 +2,27 @@ require 'rails_helper'
 
 describe 'GET /api/v1/recipe/:recipe_id' do
   it 'can get a single recipe and all ingredients' do
-    get '/api/v1/recipes/Quick-chicken-enchilada-soup-350936'
+    VCR.use_cassette('single-recipe') do
+      get '/api/v1/recipes/Quick-chicken-enchilada-soup-350936'
 
-    recipe = JSON.parse(response.body, symbolize_names: true)
+      recipe = JSON.parse(response.body, symbolize_names: true)
 
-    expect(recipe[:name]).to eq("Quick Chicken Enchilada Soup")
-    expect(recipe[:ingredients].length).to eq(9)
-    expect(recipe[:image]).to eq("http://lh4.ggpht.com/IVpIyL7sXXwoG8tl6lKKyY7vvDR7aLI4Ro40xhEwrh5EFgjk4yBXyaL0NeERBYZaPq0GfY_0cTbG_VoDW2PcTCk=s360")
-    expect(recipe[:servings]).to eq(4)
-    expect(recipe[:minutes]).to eq(25)
+      expect(recipe[:name]).to eq("Quick Chicken Enchilada Soup")
+      expect(recipe[:ingredients].length).to eq(9)
+      expect(recipe[:image]).to eq("http://lh4.ggpht.com/IVpIyL7sXXwoG8tl6lKKyY7vvDR7aLI4Ro40xhEwrh5EFgjk4yBXyaL0NeERBYZaPq0GfY_0cTbG_VoDW2PcTCk=s360")
+      expect(recipe[:servings]).to eq(4)
+      expect(recipe[:minutes]).to eq(25)
+    end
   end
 
   it 'returns 404 if cant find recipe' do
-    get '/api/v1/recipes/Quick-chicken-enchilada-soup-936'
+    VCR.use_cassette('bad-single-recipe') do
+      get '/api/v1/recipes/Quick-chicken-enchilada-soup-936'
 
-    result = JSON.parse(response.body, symbolize_names: true)
+      result = JSON.parse(response.body, symbolize_names: true)
 
-    expect(response.status).to eq(404)
-    expect(result[:message]).to eq("Not Found")
+      expect(response.status).to eq(404)
+      expect(result[:message]).to eq("Not Found")
+    end
   end
 end
--- spec/requests/searches/index_user_searches_spec.rb
+++ spec/requests/searches/index_user_searches_spec.rb
@@ -3,62 +3,71 @@ require 'rails_helper'
 describe 'GET /users/:token/searches' do
 
   it 'can access all users searches ordered by newest to oldest' do
-    user = create(:user)
-    search_1 = user.searches.create(keyword: 'chicken', allergies: 'soy, dairy', max_time: 35)
-    search_2 = user.searches.create(keyword: 'onion', allergies: 'soy', max_time: 15)
+    VCR.use_cassette('newest-search') do
+      user = create(:user)
+      search_1 = user.searches.create(keyword: 'chicken', allergies: 'soy, dairy', max_time: 35)
+      search_2 = user.searches.create(keyword: 'onion', allergies: 'soy', max_time: 15)
 
-    get "/api/v1/users/#{user.token}/searches?order=newest"
+      get "/api/v1/users/#{user.token}/searches?order=newest"
 
-    searches = JSON.parse(response.body, symbolize_names: true)
-    search = searches.first
+      searches = JSON.parse(response.body, symbolize_names: true)
+      search = searches.first
 
-    expect(response).to be_successful
-    expect(searches.length).to eq(2)
-    expect(search[:max_time]).to eq(search_2.max_time)
-    expect(search[:allergies]).to eq(search_2.allergies)
-    expect(search[:keyword]).to eq(search_2.keyword)
+      expect(response).to be_successful
+      expect(searches.length).to eq(2)
+      expect(search[:max_time]).to eq(search_2.max_time)
+      expect(search[:allergies]).to eq(search_2.allergies)
+      expect(search[:keyword]).to eq(search_2.keyword)
+    end
   end
 
   it 'can access all users searches ordered oldest to newest' do
-    user = create(:user)
-    search_1 = user.searches.create(keyword: 'chicken', allergies: 'soy, dairy', max_time: 35)
-    search_2 = user.searches.create(keyword: 'onion', allergies: 'soy', max_time: 15)
+    VCR.use_cassette('oldest-search') do
+      user = create(:user)
+      search_1 = user.searches.create(keyword: 'chicken', allergies: 'soy, dairy', max_time: 35)
+      search_2 = user.searches.create(keyword: 'onion', allergies: 'soy', max_time: 15)
 
-    get "/api/v1/users/#{user.token}/searches?order=oldest"
+      get "/api/v1/users/#{user.token}/searches?order=oldest"
 
-    searches = JSON.parse(response.body, symbolize_names: true)
-    search = searches.first
+      searches = JSON.parse(response.body, symbolize_names: true)
+      search = searches.first
 
-    expect(response).to be_successful
-    expect(searches.length).to eq(2)
-    expect(search[:max_time]).to eq(search_1.max_time)
-    expect(search[:allergies]).to eq(search_1.allergies)
-    expect(search[:keyword]).to eq(search_1.keyword)
+      expect(response).to be_successful
+      expect(searches.length).to eq(2)
+      expect(search[:max_time]).to eq(search_1.max_time)
+      expect(search[:allergies]).to eq(search_1.allergies)
+      expect(search[:keyword]).to eq(search_1.keyword)
+    end
   end
 
   it 'can access all users searches not ordered' do
-    user = create(:user)
-    search_1 = user.searches.create(keyword: 'chicken', allergies: 'soy, dairy', max_time: 35)
-    search_2 = user.searches.create(keyword: 'onion', allergies: 'soy', max_time: 15)
+    VCR.use_cassette('not-ordered-searches') do
+      user = create(:user)
+      search_1 = user.searches.create(keyword: 'chicken', allergies: 'soy, dairy', max_time: 35)
+      search_2 = user.searches.create(keyword: 'onion', allergies: 'soy', max_time: 15)
 
-    get "/api/v1/users/#{user.token}/searches"
+      get "/api/v1/users/#{user.token}/searches"
 
-    searches = JSON.parse(response.body, symbolize_names: true)
-    search = searches.first
+      searches = JSON.parse(response.body, symbolize_names: true)
+      search = searches.first
 
-    expect(response).to be_successful
-    expect(searches.length).to eq(2)
-    expect(search[:max_time]).to eq(search_1.max_time)
-    expect(search[:allergies]).to eq(search_1.allergies)
-    expect(search[:keyword]).to eq(search_1.keyword)
+      expect(response).to be_successful
+      expect(searches.length).to eq(2)
+      expect(search[:max_time]).to eq(search_1.max_time)
+      expect(search[:allergies]).to eq(search_1.allergies)
+      expect(search[:keyword]).to eq(search_1.keyword)
+    end
   end
 
   it 'returns 404 if no user found' do
-    get "/api/v1/users/0980324jkjlkj/searches?order=oldest"
+      VCR.use_cassette('search-not-found') do
 
-    result = JSON.parse(response.body, symbolize_names: true)
+      get "/api/v1/users/0980324jkjlkj/searches?order=oldest"
 
-    expect(response.status).to eq(404)
-    expect(result[:message]).to eq("Not Found")
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(404)
+      expect(result[:message]).to eq("Not Found")
+    end
   end
 end
