53c35e611d71d0ea84c07a57c4dacf73ef8d2293
stephen <malek.stephen44@gmail.com>
2018-12-10 18:04:38 -0700

    Remove line from vcr configure

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,7 +32,6 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/cassettes"
   config.hook_into :webmock
   config.allow_http_connections_when_no_cassette = true
-  config.filter_sensitive_data('<key>') { ENV["GITHUB_TEST_KEY"] }
 end
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
